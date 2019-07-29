3c2cc6798df6086b89f5e762f5ffb07417f82219
tlswodyd413 <tlswodyd413@gmail.com>
2019-07-29 16:01:33 +0900

    test

--- b_2588/Main.java
+++ b_2588/Main.java
@@ -1,10 +1,21 @@
 package b_2588;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num1, num2;
+		
+		num1 = scanner.nextInt();
+		num2 = scanner.nextInt();
+		
+		System.out.println( num1 * (num2 %10) );
+	
+	
 	}
 
 }
---
+++ b_2588/src/b_2588/Main.java
@@ -0,0 +1,12 @@
+package b_2588;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi");
+		
+	}
+
+}
